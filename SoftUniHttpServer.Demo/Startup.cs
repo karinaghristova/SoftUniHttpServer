b7d35a594a19e18052c2b4d29929581de69ceb47
karinaghristova <karina.hristova@abv.bg>
2022-02-06 22:13:22 +0200

    Startup refactured

--- SoftUniHttpServer.Demo/Startup.cs
+++ SoftUniHttpServer.Demo/Startup.cs
@@ -8,16 +8,6 @@ using SoftUniHttpServer.Server.Routing;
 
 public class Startup
 {
-    private const string LoginForm = @"<form action='/Login' method='POST'>
-       Username: <input type='text' name='Username'/>
-       Password: <input type='text' name='Password'/>
-       <input type='submit' value ='Log In' /> 
-    </form>";
-
-    private const string Username = "user";
-
-    private const string Password = "user123";
-
     public static async Task Main()
     {
         await new HttpServer(routes => routes
@@ -29,68 +19,11 @@ public class Startup
             .MapPost<HomeController>("/Content", c => c.DownloadContent())
             .MapGet<HomeController>("/Cookies", c => c.Cookies())
             .MapGet<HomeController>("/Session", c => c.Session())
-            //.MapGet<HomeController>("/Login", new HtmlResponse(Startup.LoginForm))
-            //.MapPost<HomeController>("/Login", new HtmlResponse("", Startup.LoginAction))
-            //.MapGet<HomeController>("/Logout", new HtmlResponse("", Startup.LogoutAction))
-            //.MapGet<HomeController>("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction))
+            .MapGet<UserController>("/Login", c => c.Login())
+            .MapPost<UserController>("/Login", c => c.LoginUser())
+            .MapGet<UserController>("/Logout", c => c.Logout())
+            .MapGet<UserController>("/UserProfile", c => c.GetUserData())
             ).Start();
     }
 
-    private static void GetUserDataAction(Request request, Response response)
-    {
-        if (request.Session.ContainsKey(Session.SessionUserKey))
-        {
-            response.Body = "";
-            response.Body += $"<h3>Currently logged-in user " + $"is with username '{Username}'</h3>";
-        }
-        else
-        {
-            response.Body = "";
-            response.Body += $"You should first log in " + $"- <a href='/Login'>Login</a></h3>";
-        }
-    }
-
-    private static void LogoutAction(Request request, Response response)
-    {
-        request.Session.Clear();
-
-        response.Body = "";
-        response.Body += "<h3>Logged out successfully!</h3>";
-    }
-
-    private static void LoginAction(Request request, Response response)
-    {
-        request.Session.Clear();
-
-        var bodyText = "";
-
-        var usernameMatches = request.Form["Username"] == Startup.Username;
-        var passwordMatches = request.Form["Password"] == Startup.Password;
-
-        if (usernameMatches && passwordMatches)
-        {
-            request.Session[Session.SessionUserKey] = "MyUserId";
-            response.Cookies.Add(Session.SessionCookieName,
-                request.Session.Id);
-            bodyText = "<h3>Logged successfully!</h3>";
-        }
-        else
-        {
-            bodyText = Startup.LoginForm;
-        }
-
-        response.Body = "";
-        response.Body += bodyText;
-    }
-
-    private static void AddFormDataAction(Request request, Response response)
-    {
-        response.Body = "";
-
-        foreach (var (key, value) in request.Form)
-        {
-            response.Body += $"{key} - {value}";
-            response.Body += Environment.NewLine;
-        }
-    }
 }
