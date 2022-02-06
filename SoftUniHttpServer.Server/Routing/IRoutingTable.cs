cdfa446dda63f1d1038dd7cb1ac4e91bc636cf78
karinaghristova <karina.hristova@abv.bg>
2022-02-06 21:42:00 +0200

    IRoutingTable interface modified

--- SoftUniHttpServer.Server/Routing/IRoutingTable.cs
+++ SoftUniHttpServer.Server/Routing/IRoutingTable.cs
@@ -4,8 +4,8 @@ namespace SoftUniHttpServer.Server.Routing
 {
     public interface IRoutingTable
     {
-        IRoutingTable Map(string url, Method method, Response response);
-        IRoutingTable MapGet(string url, Response response);
-        IRoutingTable MapPost(string url, Response response);
+        IRoutingTable Map(Method method, string path, Func<Request, Response> responseFunction);
+        //IRoutingTable MapGet(string path, Func<Request, Response> responseFunction);
+        //IRoutingTable MapPost(string path, Func<Request, Response> responseFunction);
     }
 }
