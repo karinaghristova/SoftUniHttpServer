087c79904a71febc9661f216132edf3e2edebf50
karinaghristova <karina.hristova@abv.bg>
2022-02-06 21:43:29 +0200

    RoutingTable extension class

---
+++ SoftUniHttpServer.Server/Routing/RoutingTableExtension.cs
@@ -0,0 +1,29 @@
+﻿using SoftUniHttpServer.Server.Controllers;
+using SoftUniHttpServer.Server.HTTP;
+
+namespace SoftUniHttpServer.Server.Routing
+{
+    public static class RoutingTableExtension
+    {
+        public static IRoutingTable MapGet<TController>
+            (this IRoutingTable routingTable, 
+            string path, 
+            Func<TController, Response> controllerFunction) where TController : Controller
+        => routingTable.Map(
+            Method.Get, 
+            path, 
+            request => controllerFunction(CreateController<TController>(request)));
+
+        public static IRoutingTable MapPost<TController>
+            (this IRoutingTable routingTable,
+            string path,
+            Func<TController, Response> controllerFunction) where TController : Controller
+        => routingTable.Map(
+            Method.Post,
+            path,
+            request => controllerFunction(CreateController<TController>(request)));
+
+        private static TController CreateController<TController>(Request request) 
+            => (TController)Activator.CreateInstance(typeof(TController), new[] { request });
+    }
+}
