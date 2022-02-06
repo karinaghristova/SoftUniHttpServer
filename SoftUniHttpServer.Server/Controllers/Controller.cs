5927f6e1a85994ec86e539df9a40de53e3a52069
karinaghristova <karina.hristova@abv.bg>
2022-02-06 23:23:53 +0200

    Added view response with model

--- SoftUniHttpServer.Server/Controllers/Controller.cs
+++ SoftUniHttpServer.Server/Controllers/Controller.cs
@@ -37,6 +37,8 @@ namespace SoftUniHttpServer.Server.Controllers
 
         protected Response View([CallerMemberName] string viewName = "")
             => new ViewResponse(viewName, GetControllerName());
+        protected Response View(object model, [CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName(), model);
 
         private string GetControllerName()
             => this.GetType().Name.Replace(nameof(Controller), string.Empty);
